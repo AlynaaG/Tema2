8758467d9236926782f34bb7f2b6af91f6abc387
AlynaaG <alynaa.giurca@yahoo.com>
2023-10-24 16:43:20 +0300

    jstema

---
+++ index.js
@@ -0,0 +1,28 @@
+/*2. Scrieți o funcție numită tellFortune care:
+
+primește 4 argumente: numărul de copii (N), numele partenerului (Z), locația geografică (Y), locul de muncă (X).
+afișează pe ecran viitorul dvs. în felul următor: "Vei fi un X în Y, căsătorit cu Z și vei avea N copii."
+În rezolvarea problemei, vom încerca să abordăm șirurile templetizate (${variabila} este o variabila.).
+
+Exemplu 1:
+
+Input: numarCopii: 3, numePartener: “Emanuel”, locatieGeografica: “Italia”, locMunca: “Programator”
+
+Output: “Vei fi un **Programator** în **Italia**, căsătorit cu **Emanuel** și vei avea **3** copii.”
+
+*/
+
+function tellFortune(N, Z, Y, X) {
+  return (
+    " Vei fi un " +
+    X +
+    " in " +
+    Y +
+    " casatorit cu " +
+    Z +
+    " si vei avea " +
+    N +
+    " copii "
+  );
+}
+tellFortune("3", "Emanuel", "Italia", "Programator");
